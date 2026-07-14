f86363f0a476307b342e4698f052523fa7af9ae4
gemasaurav <gema.saurav@gmail.com>
2026-07-14 09:25:40 +0530

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,1 +1,51 @@
+const dictionary = {
 
+apple:{
+
+english:"A fruit.",
+
+hindi:"सेब",
+
+type:"Noun",
+
+synonyms:"Fruit",
+
+antonyms:"None",
+
+example:"I eat an apple every day."
+
+},
+
+beautiful:{
+
+english:"Pleasing in appearance.",
+
+hindi:"सुंदर",
+
+type:"Adjective",
+
+synonyms:"Pretty, Lovely, Attractive",
+
+antonyms:"Ugly",
+
+example:"She is beautiful."
+
+},
+
+country:{
+
+english:"A nation with its own government.",
+
+hindi:"देश",
+
+type:"Noun",
+
+synonyms:"Nation, State",
+
+antonyms:"None",
+
+example:"India is a country."
+
+}
+
+};
