1297728e2e5718e7ee0a0bfe6139940b52eadb18
gemasaurav <gema.saurav@gmail.com>
2026-07-14 09:26:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
+const searchBtn =
+document.getElementById("searchBtn");
 
+const result =
+document.getElementById("result");
+
+searchBtn.addEventListener(
+"click",
+function(){
+
+let word =
+document.getElementById(
+"wordInput"
+).value.toLowerCase();
+
+if(dictionary[word]){
+
+let data =
+dictionary[word];
+
+result.innerHTML =
+
+`
+<h2>${word}</h2>
+
+<p><b>Type:</b>
+${data.type}</p>
+
+<p><b>English Meaning:</b>
+${data.english}</p>
+
+<p><b>Hindi Meaning:</b>
+${data.hindi}</p>
+
+<p><b>Synonyms:</b>
+${data.synonyms}</p>
+
+<p><b>Antonyms:</b>
+${data.antonyms}</p>
+
+<p><b>Example:</b>
+${data.example}</p>
+
+`;
+
+}
+
+else{
+
+result.innerHTML =
+
+"<h2>Word Not Found</h2>";
+
+}
+
+});
