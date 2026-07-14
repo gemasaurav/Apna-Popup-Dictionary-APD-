5ceb9bd205b1740ccbdf6d7234d620494948852e
gemasaurav <gema.saurav@gmail.com>
2026-07-15 00:16:36 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,58 +1,79 @@
 let selectedWord = "";
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener(
+"DOMContentLoaded",
+function(){
 
-const floatingBtn = document.getElementById("floatingBtn");
-const popupBox = document.getElementById("popupBox");
-const popupWord = document.getElementById("popupWord");
-const popupSearch = document.getElementById("popupSearch");
-const closePopup = document.getElementById("closePopup");
+const floatingBtn =
+document.getElementById("floatingBtn");
 
-floatingBtn.style.display = "none";
+const popupBox =
+document.getElementById("popupBox");
 
-document.addEventListener("selectionchange", function(){
+const closePopup =
+document.getElementById("closePopup");
 
-let text = window.getSelection().toString().trim();
+floatingBtn.style.display =
+"none";
+
+document.addEventListener(
+"selectionchange",
+function(){
+
+let text =
+window.getSelection()
+.toString()
+.trim();
 
 if(text.length > 0){
 
 selectedWord = text;
 
-floatingBtn.style.display = "flex";
+floatingBtn.style.display =
+"flex";
 
 }else{
 
-floatingBtn.style.display = "none";
+floatingBtn.style.display =
+"none";
 
 }
 
 });
 
-floatingBtn.addEventListener("click", function(){
+floatingBtn.addEventListener(
+"click",
+function(){
 
 if(!selectedWord) return;
 
-popupBox.style.display = "block";
+popupBox.style.display =
+"block";
 
-popupWord.value = selectedWord;
+searchWord(selectedWord);
 
-popupSearch.click();
-
-floatingBtn.style.display = "none";
-
-});
-
-closePopup.addEventListener("click", function(){
-
-popupBox.style.display = "none";
+floatingBtn.style.display =
+"none";
 
 });
 
-window.addEventListener("click", function(e){
+closePopup.addEventListener(
+"click",
+function(){
+
+popupBox.style.display =
+"none";
+
+});
+
+window.addEventListener(
+"click",
+function(e){
 
 if(e.target === popupBox){
 
-popupBox.style.display = "none";
+popupBox.style.display =
+"none";
 
 }
 
